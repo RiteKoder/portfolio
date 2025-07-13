43eca42bec44b0ade9c3268beb4c03e0a7a124b9
RiteshKrPanda <riteshkrpandawork@gmail.com>
2025-07-13 19:19:04 +0530

    fixed game

--- JSgame/script.js
+++ JSgame/script.js
@@ -1,3 +1,9 @@
+function playerwon() {
+  player1.style.backgroundColor = "green";
+  score.innerText = "you won";
+  actScore = 0;
+}
+
 const player1 = document.getElementById("player1");
 
 let actScore = 0;
@@ -6,19 +12,16 @@ let score = document.getElementById("score");
 player1.addEventListener("mousemove", () => {
   player1.style.backgroundColor = "yellow";
 
-  actScore++;
+  actScore = actScore + 1;
+  actScore = Math.round(actScore);
   console.log(actScore);
   score.innerText = actScore;
 });
 player1.addEventListener("mouseleave", () => {
-  player1.style.backgroundColor = "blue";
-  if (actScore > 1) {
-    score.innerText = "you won";
-    // const area = document.getElementsById("playarea");
-    // area.style.backgroundColor = "green";
+  if (actScore > 50) {
+    playerwon();
   } else {
-    score.innerText = "your score is " + actScore;
+    score.innerText = "You lose , your score is " + actScore;
+    actScore = 0;
   }
-  actScore = 0;
-  // score.innerText = actScore;
 });
