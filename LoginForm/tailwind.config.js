b3b6161aadc0877390a87e0f7afe784dad184a39
RiteshKrPanda <riteshkrpandawork@gmail.com>
2025-09-06 21:42:15 +0530

    c

---
+++ LoginForm/tailwind.config.js
@@ -0,0 +1,14 @@
+    // tailwind.config.js
+    module.exports = {
+      theme: {
+        extend: {
+          colors: {
+            'custom-blue': '#1a2b3c', // Add a new custom color
+            'primary-brand': '#ff6600', // Another custom color
+            // You can also extend existing Tailwind colors with new shades
+            'black-dark': '#00000050', // Custom dark black color
+          },
+        },
+      },
+      plugins: [],
+    };
